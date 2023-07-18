21bd7a695d62480f6808e92d9368a22ef40915d5
Julian 41455100+thieleju@users.noreply.github.com <thiele.juli@gmail.com>
2023-07-18 19:27:02 +0200

    Added lichesselo route and docker workflow

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Base image
+FROM node:20-alpine
+
+# Create and set the working directory
+WORKDIR /app
+
+# Copy package.json and package-lock.json to the working directory
+COPY package*.json ./
+
+# Install dependencies
+RUN npm install
+
+# Copy the rest of the server code
+COPY . .
+
+# Expose the port that the Express app listens on
+EXPOSE 3000
+
+# Start the Express app
+CMD [ "npm", "start" ]
---
+++ routes/lichesselo.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
+
+const lichessurl = "https://lichess.org/api/user";
+
+router.get("/:mode/:username", async (req, res) => {
+  try {
+    const cleanMode = req.paramString("mode");
+    const cleanUsername = req.paramString("username");
+
+    const apiUrl = `${lichessurl}/${cleanUsername}`;
+
+    const { data } = await axios.get(apiUrl);
+
+    let response = "Invalid mode!";
+
+    switch (cleanMode) {
+      case "bullet":
+        response = data.perfs.bullet.rating;
+        break;
+      case "blitz":
+        response = data.perfs.blitz.rating;
+        break;
+      case "rapid":
+        response = data.perfs.rapid.rating;
+        break;
+      case "classical":
+        response = data.perfs.classical.rating;
+        break;
+      case "puzzle":
+        response = data.perfs.puzzle.rating;
+    }
+    log(`Request for ${cleanUsername} (${cleanMode}) => ${response}`);
+    res.send(String(response));
+  } catch (err) {
+    log(err, err.response.statusText);
+    res.send("Invalid request!");
+  }
+});
+
+module.exports = router;
+
+// log with multiple arguments
+function log(...msg) {
+  console.log(`${new Date().toISOString()} | ${msg}`);
+}
