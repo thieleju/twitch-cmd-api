fdf19e2d145fcb1ff12fb6cb94f0c4bc9a7fe5f7
Julian <thiele.juli@gmail.com>
2023-07-19 18:45:29 +0200

    Increased rate limit to 500

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // max 100 requests per windowMs
+  max: 500, // max 100 requests per windowMs
 });
 
 app.use(limiter);
